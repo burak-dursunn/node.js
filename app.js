1fc1c5740e826edb0d241e3739c1e057859c2f18
burak-dursunn <dursunburak046@gmail.com>
2025-09-01 11:47:14 +0300

    partials and create process

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.listen(3000);
 
 // app.use(express.static(path.join(__dirname, 'views')));
 
-
+const blogs = [
+{title: 'Blog1', snippet: 'lorem ipsum dolor sit amet'},
+{title: 'Blog2', snippet: 'consectetur adipiscing elit'},
+{title: 'Blog3', snippet: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+];
 // routers
 app.get('/', (req,res) => {
-    const blogs = [
-    {title: 'Blog1', snippet: 'lorem ipsum dolor sit amet'},
-    {title: 'Blog2', snippet: 'consectetur adipiscing elit'},
-    {title: 'Blog3', snippet: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
-];
     res.render('index', { title: 'home', blogs});
 })
 
@@ -29,8 +30,15 @@ app.get('/about-me', (req,res) => {
 })
 
 app.get('/blogs/create', (req,res) => {
-    res.render('create', { title: 'Create Blog'});
+    res.render('create', { title: 'Create new Blog'})
 })
+
+app.post('/blogs/create', (req,res) => {
+    const { title, snippet} = req.body;
+    blogs.push({ title: title, snippet: snippet});
+    res.redirect('/');
+})
+
 
 // 404
 app.use((req,res) => {
